0561473b034a679238a3b5b1775a15b413962ab0
Bohtaro <bohtarovaha@gmail.com>
2022-10-18 15:09:14 +0300

    Initial commit

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,39 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users-db")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public User() {}
+
+    public User(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
